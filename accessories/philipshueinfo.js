3cf114269bf0322b350fed71ce3944220e7d3db0
Rens Verhoeven <rens@renssies.nl>
2014-11-08 18:23:51 +0100

    Added philips hue info and an example

---
+++ accessories/philipshueinfo.js
@@ -0,0 +1,6 @@
+var exports = module.exports = {};
+
+exports.philipsHueIP = "<The ip of your hue bridge>"; //The IP of your hue bridge
+exports.philipsHueUsername = "homekitserver"; //The username to communicate with the hue bridge
+exports.philipsHueManufacturer = "Philips"; //The manufacturer for the lights, not very important
+exports.philipsHueDevicePin = "031-45-154"; //The pin you have to enter when connecting the homekit light
